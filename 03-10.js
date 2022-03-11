eb2799ade81df85e15ec51ff6c25a29b19e15580
yinghong <thamyinghong@gmail.com>
2022-03-10 23:01:32 -0500

    post meeting, note the JS has lots of errors, comment as needed

--- 03-10.js
+++ 03-10.js
@@ -5,7 +5,11 @@ var c = 3;
 {
   let b = 4;
   var c = 5;
+  let d = 6;
 }
+
+// no good, because out of scope of defn of d
+//console.log(d);
 
 console.log('Redeclaring:');
 console.log('b: ', b);
@@ -16,14 +20,67 @@ console.log('c: ', c);
   c = 7;
 }
 
-console.log('Redefine:');
+console.log('Reassign:');
 console.log('b: ', b);
 console.log('c: ', c);
 
 
+let e = 0;
+{
+  let e = 1;
+  //let e = 5; // bad, cannot redeclare
+  {
+    let e = 2; // will be reassigned to 3
+    {
+      e = 3;
+      console.log('e: ', e); // 3
+    }
+    console.log('e: ', e); // 3
+  }
+  console.log('e: ', e); // 1
+}
+console.log('e: ', e); // 0
+
+
+// funciton namespace
+
+let v = 100;
+function recur(n) {
+  if (n < 0)
+    return;
+  v++;
+  recur(n-1);
+}
+
+//recur(10);
+//recur(10):
+//  
+//  recur(9):
+//    let a = 0;
+//    recur(8):
+//      ...
+//
+
+
+// {
+//   {
+//     let a = 0;
+//   }
+//   {
+//     a = 5; // error: no such variable
+//   }
+// }
+
+//===============================================================
 // callbacks
 
 console.log('start timeout');
+
+() => { ... }
+let fn = (a) => { console.log(a); }
+function fn(a) {
+  console.log(a);
+}
 
 setTimeout(() => {
   console.log('after 1 second');
@@ -41,14 +98,31 @@ let myFunc2 = myFunc;
 
 // reading files
 
-//fetch('03-10.txt')
-//  .then(response => response.text())
-//  .then(text => console.log(text));
+function readFile(filename) {
+  fetch(filename)
+    .then( (response) => { return response.text();} )
+    .then( doSomethingToText );
+}
 
+function doSomethingToText(filetext) {
+  console.log(filetext);
+}
+
+fetch API {
+  send request to whatever (internet/filesystem))
+    for the thing you want
+  .. wait ..
+  data received, in some special formj
+pass to first .then callback
+data => data.text()
+pass output of that to next .then callback
+data.text() =>  console.log(data.text())
 
 // applies func(-,-) to elements from arr1, arr2
 // returns new array
 // if lengths not equal, only do up to min length
+// func expect two input variables, of type
+// same as entries of arr1,arr2
 function combineTwoArrays(arr1, arr2, func) {
   const minlength = Math.min(arr1.length, arr2.length);
   const ans = [];
@@ -63,6 +137,22 @@ let arr2 = [5,6,7,8];
 let func = (k,l) => {
   return k+l;
 }
+let func2 = (k,l) => {
+  return k*l;
+}
 
-console.log(combineTwoArrays(arr1, arr2, func));
+console.log('arr1: ', arr1);
+console.log('arr2: ', arr2);
+console.log('ans: ', combineTwoArrays(arr1, arr2, func));
+
+
+////THREE.js
+//const v1 = new Vector3(0,1,0);
+//const v2 = new Vector3(2,-1,1);
+//const v3 = v1.add(v2);
+//
+//function addVectors(a,b) {
+//  return a.add(b);
+//}
+
 
